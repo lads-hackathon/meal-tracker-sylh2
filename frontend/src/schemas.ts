7431920579661265610cec48f9d2ec9be5081b7b
YoungerMax <60242131+YoungerMax@users.noreply.github.com>
2022-12-30 22:51:18 -0500

    Update schema

--- frontend/src/schemas.ts
+++ frontend/src/schemas.ts
@@ -8,14 +8,12 @@ export interface NutritionFacts {
     calories: number;
     total_fat_grams: number;
     saturated_fat_grams: number;
-    trans_fat_grams: number;
     cholesterol_milligrams: number,
     sodium_milligrams: number;
     total_carb_grams: number;
     dietary_fiber_grams: number;
     sugars_grams: number;
     proteins_grams: number;
-    vitamins: VitaminFacts[];
 }
 
 export interface FoodNutritionFacts {
